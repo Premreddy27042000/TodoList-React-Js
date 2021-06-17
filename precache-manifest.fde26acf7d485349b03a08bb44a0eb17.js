cb6a363d3b0c9d9c5361b2ca18b662fd08a5e57f
Code Sandbox <hello@codesandbox.io>
2021-06-17 11:00:06 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.fde26acf7d485349b03a08bb44a0eb17.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b51cfd4ca8fcae514975e271e43212",
+    "url": "https://premreddy27042000.github.io/csb-ntuz9/index.html"
+  },
+  {
+    "revision": "9302e28affcbd310209f",
+    "url": "https://premreddy27042000.github.io/csb-ntuz9/static/js/2.17e1ab02.chunk.js"
+  },
+  {
+    "revision": "c6713c14076aacb056c1",
+    "url": "https://premreddy27042000.github.io/csb-ntuz9/static/js/main.c5ba9caf.chunk.js"
+  },
+  {
+    "revision": "613d5d852f1faa3dc14c",
+    "url": "https://premreddy27042000.github.io/csb-ntuz9/static/js/runtime-main.a4eb6c3f.js"
+  }
+]);
